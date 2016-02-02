9fcc98069f10ce6b4380ac7655f50203afe5a151
nicjo <nicjorgensen@yahoo.ca>
2016-02-02 23:24:44 +0000

    Fortune teller using process.argv

--- fortune-teller.js
+++ fortune-teller.js
@@ -1,3 +1,10 @@
 var fortune = require("./library/fortune.js")
 
-console.log(fortune.getFortune())
+process.argv.forEach(function(val, index, array){
+    
+    if (index === 2) {
+        for (var i = 0; i < parseInt(val); i++) {
+            console.log(fortune.getFortune());
+        }
+    }
+});
--- library/fortune.js
+++ library/fortune.js
@@ -13,4 +13,3 @@ module.exports = {getFortune: function getFortune () {
     }
     
 };
-
