a411b7c0e73ec9f29ee272bd366c64f2ceb2ea00
nicjo <nicjorgensen@yahoo.ca>
2016-02-02 23:55:46 +0000

    My first NodeJS servergit status!

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+    var requestListener = function (req, response) {
+     
+             response.end("Hello World!");
+    }
+
+ 
+ var server = http.createServer(requestListener);
+ 
+ server.listen(process.env.PORT, process.env.IP); 
